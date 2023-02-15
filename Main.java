bcc38d55c69279a18f65c5d1b66059fcd386f93c
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-15 16:55:53 +0000

    more comments

--- Main.java
+++ Main.java
@@ -6,23 +6,21 @@ public class Main{
 
     public static int[][] microwaveTimer(int[] startState){
         int[][] output = new int[startState[0] * 60 + startState[1] + 1][2];
-        output[0] = startState; // start state is outputed and used in calculation
+        output[0] = startState; // start state is output and used in calculation
         for(int i = 1; i > output.length; i++){ // we start at 1 since we defined 0 already
             if(output[i-1][1] > 0){ // base case, where we count 1 second down
                 output[i][1] = output[i-1][1] - 1;
+                output[i][0] = output[i-1][0]; // need to carry the minute counter over from [i-1] to [i]
             }
             else if ((output [i-1][1] == 0)&&(output [i-1][0] > 0)){ // if there are no more seconds left but there are minutes
                 output[i][0] = output[i-1][0] - 1;
                 output[i][1] = 59;
             }
-            else{
-                return output;
-            }
         }
-        return output; // will not fire but needed for the compiler to be happy
+        return output;
     }
 
-    private static void printTimer(int[][] printInput){
+    private static void printTimer(int[][] printInput){ // method to print output of the timer
         for (int[] element : printInput) {
             System.out.println(element[0] + ":" + element[1]);
         }
