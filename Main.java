3f1a5c510fca88d9bbd61bc547f162ef6715b31f
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-15 16:46:37 +0000

    added printTimer method

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main{
     public static void main(String[] args){
         int[] testInput = {1, 80};
-        System.out.println(microwaveTimer(testInput));
+        printTimer(microwaveTimer(testInput));
     }
 
     public static int[][] microwaveTimer(int[] startState){
         int[][] output = new int[startState[0] * 60 + startState[1] + 1][2];
-        for(int i = 0; i > output.length + 1; i++){
+        output[0] = startState;
+        for(int i = 1; i > output.length + 1; i++){ // we start at 1 since we defined 0 already
             if(output[i-1][1] > 0){ // base case, where we count 1 second down
                 output[i][1] = output[i-1][1] - 1;
             }
@@ -19,5 +20,11 @@ public class Main{
             }
         }
         return output; // will not fire but needed for the compiler to be happy
+    }
+
+    private static void printTimer(int[][] printInput){
+        for (int[] element : printInput) {
+            System.out.println(element[0] + ":" + element[1]);
+        }
     }
 }
