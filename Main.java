14b1e75e0e4d04987b9d0f5bbe9950e212c4fd8b
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-03-02 20:35:34 +0000

    more testing

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String[] args){
         int[] testInput = {1, 70};
+        int[] testInput = {1, 50};
         printTimer(microwaveTimer(testInput));
     }
 
